1248dd97203b63ffc24b79aebab54f5435f218d3
Urs Lang <urs.lang@nemundo.ch>
2025-04-10 16:54:26 +0200

    .

--- web/api/question.php
+++ web/api/question.php
@@ -60,7 +60,7 @@ header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Methods: "GET, POST, PUT, DELETE,OPTIONS"');
 //Access-Control-Allow-Methods
 
-header('Content-Type: application/json; charset=utf-8');
+//header('Content-Type: application/json; charset=utf-8');
 header("HTTP/1.1 200 OK");
 
 //http_response_code(200);
